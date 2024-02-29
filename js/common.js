b0d25266c8d21e19e3808978309ddc2478e6878b
ch0hyem1n <161679769+ch0hyem1n@users.noreply.github.com>
2024-02-29 14:10:05 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(function(){
+  $(".ham").click(function () {
+    $(".submenu").stop().animate({ right: '0' }, 1000)
+  })
+  
+  $(".close").click(function () {
+    $(".submenu").stop().animate({ right: '-100%' }, 1000)
+  })
+
+  $(".slide").slick({
+    slidesToShow: 1,
+    autoplay: true,
+    infinite: true,
+    responsive: [
+        // 배열 안에 객체를 넣는다
+        {
+            // 반응형 잡는 법
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+
+
+    ]
+})
+
+///////
+});
